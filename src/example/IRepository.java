76f0acd196a2566e5ef94c5f2dee14d440b9e4ac
M.Furkan <demirdelen12012@gmail.com>
2022-11-17 11:22:11 +0300

    Generics

---
+++ src/example/IRepository.java
@@ -0,0 +1,14 @@
+package example;
+
+import java.util.List;
+
+public interface IRepository <T extends BaseClass> {
+	
+	//CRUD operations
+	void save(T t);
+	T find(T t);
+	void update(T t);
+	void delete(T t);
+	List<T> getAll();
+
+}
---
+++ src/example/OtherClassRepository.java
@@ -0,0 +1,9 @@
+package example;
+//OtherClass BaseClass!ı extend etmediği için UpperBound kuralına uymuyor ve IRepository'yi
+//implement edemiyoruz
+
+//package com.tpe.generics.example;
+//
+//public class OtherClassRepository implements IRepository<OtherClass> {
+//
+//}
